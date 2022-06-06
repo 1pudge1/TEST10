196d1b2ae00cfd31e2b193dd998492f9973b429c
Evgeny Mandrik <jenya.mandrik@gmail.com>
2022-06-06 14:05:17 +0300

    test

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
+from . import views
+app_name = 'blog'
 urlpatterns = [
- path('admin/', admin.site.urls),
- path('blog/', include('blog.urls', namespace='blog')),
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
+
